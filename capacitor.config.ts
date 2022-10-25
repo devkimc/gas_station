6487fc793eb53bfa6a18dd28fcf6859a7ddc54d1
linkrws <kws60000@naver.com>
2022-10-25 19:09:14 +0900

    chore: capacitor 설치

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gas_station',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
